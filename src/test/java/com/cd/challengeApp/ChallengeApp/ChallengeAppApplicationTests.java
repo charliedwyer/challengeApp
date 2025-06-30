b9b498a318ee4c5651372100e3415061818283ce
DWYER316 <dwyer316@d.umn.edu>
2025-06-29 20:43:07 -0500

    first commit

---
+++ src/test/java/com/cd/challengeApp/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.challengeApp.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
