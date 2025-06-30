6f92e3ff71a3902a6fd23c033ffcd5070f9f826b
DWYER316 <dwyer316@d.umn.edu>
2025-06-29 21:29:27 -0500

    Create Controller and GET challenges end point

---
+++ src/main/java/com/cd/challengeApp/ChallengeApp/ChallengeController.java
@@ -0,0 +1,24 @@
+package com.cd.challengeApp.ChallengeApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ChallengeController {
+
+    private List<Challenge> challenges =
+            new ArrayList<>();
+
+    public ChallengeController() {
+        Challenge challenge1 = new Challenge(1L, "January", "Learn a new programming language");
+        challenges.add(challenge1);
+    }
+
+    @GetMapping("/challenges")
+    public List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+}
