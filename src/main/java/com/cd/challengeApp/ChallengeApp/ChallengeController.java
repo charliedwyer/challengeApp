75ef07defebb5270ebcc90e506a651666d596833
charliedwyer <cdwr17@gmail.com>
2025-07-05 17:26:27 -0500

    Create post request for challenge endpoint

--- src/main/java/com/cd/challengeApp/ChallengeApp/ChallengeController.java
+++ src/main/java/com/cd/challengeApp/ChallengeApp/ChallengeController.java
@@ -1,6 +1,8 @@
 package com.cd.challengeApp.ChallengeApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -20,5 +22,11 @@ public class ChallengeController {
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges() {
         return challenges;
+    }
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge) {
+        challenges.add(challenge);
+        return "Challenge Added Successfully";
     }
 }
